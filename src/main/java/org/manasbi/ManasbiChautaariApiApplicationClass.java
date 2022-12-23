36cd6027aff33cc02be9af57eb502b90e988b97f
manasbiparajuli <parajulimanasbi@gmail.com>
2022-12-23 17:43:09 -0500

    Initialized Spring Boot repository for Manasbi Chautaari API

---
+++ src/main/java/org/manasbi/ManasbiChautaariApiApplicationClass.java
@@ -0,0 +1,11 @@
+package org.manasbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManasbiChautaariApiApplicationClass {
+    public static void main(String[] args) {
+        SpringApplication.run(ManasbiChautaariApiApplicationClass.class, args);
+    }
+}
